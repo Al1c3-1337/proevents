07e1913f0b13c95d25f30e98d85b90c59a620092
shaap <sebastianhaap@googlemail.com>
2020-04-25 19:26:23 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -2,8 +2,8 @@
 $sql_host = "localhost";
 $sql_db = 	"booking";
 $sql_user = "booking";
-$sql_pass = "j9p9!Gu4";
-$admin_pass = "inwcb4";
+$sql_pass = "";
+$admin_pass = "";
 $db = new PDO('mysql:host='.$sql_host.';dbname='.$sql_db.';charset=utf8', $sql_user, $sql_pass);
 
 ?>
